97d84632be154c597623420f3eedae77aa5aab53
Jsilvermist <jsilvermist@silverlinkz.net>
2019-07-15 16:40:21 -0400

    Refactor reel icons to ReelIcons

--- src/slotmachine/Controller.java
+++ src/slotmachine/Controller.java
@@ -7,7 +7,6 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
@@ -17,7 +16,7 @@ public class Controller implements Initializable {
   @FXML private ImageView imageBlock1;
   @FXML private ImageView imageBlock2;
   @FXML private ImageView imageBlock3;
-  private ImageView[] imageBlocks = new ImageView[3];
+  private ImageView[] imageBlocks;
 
   @FXML private Text moneyText;
   @FXML private Text resultText;
@@ -29,33 +28,22 @@ public class Controller implements Initializable {
 
   private DecimalFormat df = new DecimalFormat("0.00");
 
-  private Image[] images = new Image[8];
-  private int[] selection = new int[3];
+  private ReelIcons[] selection = new ReelIcons[3];
   private double totalMoney = 50.0;
   private int betAmount = 0;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     populateImagesBlocks();
-    populateImages();
     populateText();
   }
 
   private void populateImagesBlocks() {
-    imageBlocks[0] = imageBlock1;
-    imageBlocks[1] = imageBlock2;
-    imageBlocks[2] = imageBlock3;
-  }
-
-  private void populateImages() {
-    images[0] = new Image("/assets/lemon.png");
-    images[1] = new Image("/assets/cherry.png");
-    images[2] = new Image("/assets/watermelon.png");
-    images[3] = new Image("/assets/diamond.png");
-    images[4] = new Image("/assets/crown.png");
-    images[5] = new Image("/assets/bar.png");
-    images[6] = new Image("/assets/seven.png");
-    images[7] = new Image("/assets/ten.png");
+    imageBlocks = new ImageView[] {
+      imageBlock1,
+      imageBlock2,
+      imageBlock3
+    };
   }
 
   private void populateText() {
@@ -66,22 +54,8 @@ public class Controller implements Initializable {
 
   private void spin() {
     for (int i = 0; i < selection.length; i++) {
-      selection[i] = (int)(Math.random() * images.length);
-      imageBlocks[i].setImage(images[selection[i]]);
-    }
-  }
-
-  private int getScoreWeight(int number) {
-    switch (number) {
-      case 0: return 15;
-      case 1: return 20;
-      case 2: return 25;
-      case 3: return 40;
-      case 4: return 50;
-      case 5: return 75;
-      case 6: return 100;
-      case 7: return 150;
-      default: return -1;
+      selection[i] = ReelIcons.getRandom();
+      imageBlocks[i].setImage(selection[i].IMAGE);
     }
   }
 
@@ -89,40 +63,39 @@ public class Controller implements Initializable {
     resultTextContainer.setVisible(true);
     resultTextContainer.setManaged(true);
 
-    int numbersMatched = -1;
-    int matchedNumber = -1;
+    ReelIcons matchedIcon = null;
+    int numberOfMatchingIcons = 0;
 
     if (selection[0] == selection[1] && selection[0] == selection[2]) {
       // Matched 3
-      numbersMatched = 3;
-      matchedNumber = selection[0];
+      matchedIcon = selection[0];
+      numberOfMatchingIcons = 3;
     } else if (selection[0] == selection[1] || selection[0] == selection[2]) {
       // Matched 2
-      numbersMatched = 2;
-      matchedNumber = selection[0];
+      matchedIcon = selection[0];
+      numberOfMatchingIcons = 2;
     } else if (selection[1] == selection[2]) {
       // Matched 2
-      numbersMatched = 2;
-      matchedNumber = selection[1];
+      matchedIcon = selection[1];
+      numberOfMatchingIcons = 2;
     }
 
     double winnings = 0;
 
-    if (numbersMatched == 3) {
-      winnings = ((getScoreWeight(matchedNumber) / 10.0) * betAmount);
-    } else if (numbersMatched == 2) {
-      winnings = ((getScoreWeight(matchedNumber) / 30.0) * betAmount);
-    } else {
-      winnings = -betAmount;
+    if (numberOfMatchingIcons == 3) {
+      winnings = (matchedIcon.SCORE / 10.0) * betAmount;
+    } else if (numberOfMatchingIcons == 2) {
+      winnings = (matchedIcon.SCORE / 30.0) * betAmount;
     }
 
     if (winnings > 0) {
-      resultText.setText("You won $" + df.format(Math.abs(winnings)) + ".");
+      resultText.setText("You bet $" + betAmount + " and won $" + df.format(winnings) + ".");
+      totalMoney += (winnings - betAmount);
     } else {
-      resultText.setText("You lost $" + df.format(Math.abs(winnings)) + ".");
+      resultText.setText("You bet $" + betAmount + " and lost.");
+      totalMoney -= betAmount;
     }
 
-    totalMoney += winnings;
     moneyText.setText(df.format(totalMoney));
   }
 
---
+++ src/slotmachine/ReelIcons.java
@@ -0,0 +1,41 @@
+package slotmachine;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import javafx.scene.image.Image;
+
+public enum ReelIcons {
+
+  LEMON("/assets/lemon.png", 10),
+  CHERRY("/assets/cherry.png", 15),
+  WATERMELON("/assets/watermelon.png", 20),
+  DIAMOND("/assets/diamond.png", 30),
+  CROWN("/assets/crown.png", 40),
+  BAR("/assets/bar.png", 60),
+  SEVEN("/assets/seven.png", 80),
+  TEN("/assets/ten.png", 100);
+
+  // Static
+
+  private static final List<ReelIcons> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
+  private static final int SIZE = VALUES.size();
+  private static final Random RANDOM = new Random();
+
+  public static ReelIcons getRandom()  {
+    return VALUES.get(RANDOM.nextInt(SIZE));
+  }
+
+  // Non-static
+
+  public final int SCORE;
+  public final Image IMAGE;
+
+  ReelIcons(String assetPath, int scoreWeight) {
+    SCORE = scoreWeight;
+    IMAGE = new Image(assetPath);
+  }
+
+}
